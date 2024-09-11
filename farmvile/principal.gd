23cd281565d38a1ae89f38c6c06c3d2f5f05e5ac
Gustavo Andrade <gustavo20martinss@gmail.com>
2024-09-11 16:21:28 -0300

    codigo plantação update

--- farmvile/principal.gd
+++ farmvile/principal.gd
@@ -1,33 +1,28 @@
 extends Node2D
-
+var abobora = preload("res://abobora.tscn")
 var canteiro = preload("res://canteiro.tscn")
 var tomate = preload("res://tomate.tscn")
-var canteiro_instance = null  # Variável para armazenar a instância do canteiro
-
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	# Criação do canteiro ao pressionar "w" 
 	if Input.is_action_just_pressed("w"):
-		# Instanciar o canteiro e posicioná-lo
-		canteiro_instance = canteiro.instantiate()
-		canteiro_instance.position = $Didi.position  # Posicionar o canteiro em relação ao jogador Didi
-		add_child(canteiro_instance)
+		var instance = canteiro.instantiate()
+		instance.position = $Didi.position
+		add_child(instance)
+		pass
 		
-		# Adicione um pequeno atraso ou verifique se o canteiro foi adicionado corretamente
-		#await(get_tree().create_timer(0.1), "timeout")  # Aguardar um breve momento para garantir que o canteiro foi adicionado
-
-	# Criação do tomate dentro do canteiro ao pressionar o botão "space"
-	if Input.is_action_just_pressed("space") and canteiro_instance != null:
-		# Instanciar o tomate e posicioná-lo
-		var tomate_instance = tomate.instantiate()
-		tomate_instance.position = $Didi.position  # Posicionar o tomate em relação ao jogador Didi
-		add_child(tomate_instance)
-
-
-
-
-
-	#instance.position = get_global_mouse_position()
+	if Input.is_action_just_pressed("E"):
+		var instance = abobora.instantiate()
+		instance.position = $Didi.position
+		add_child(instance)
+		pass
 		
+	if Input.is_action_just_pressed("E"):
+		var instance = tomate.instantiate()
+		instance.position = $Didi.position
+		add_child(instance)
+		pass
